e4a82823f676398a028cfb6a657827ada4dfc3cb
FacundoGonzalez2005 <facundo.gonza2005@gmail.com>
2024-08-30 22:45:13 -0300

    Modificacion en los horarios para que la pastilla se relacione con su horario real del json

--- trabajo4/js/calendario.js
+++ trabajo4/js/calendario.js
@@ -17,11 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
                 const imagenElement = horarioElement.querySelector(".imagen-pastilla img");
                 const nombreElement = horarioElement.querySelector(".texto h3");
                 const descripcionElement = horarioElement.querySelector(".texto h6");
+                const horaElements = horarioElement.querySelectorAll(".hora");
 
                 // Asigna los valores del JSON a los elementos
                 imagenElement.src = pastilla.imagen;
                 nombreElement.textContent = pastilla.nombre;
                 descripcionElement.textContent = `Dosis ${pastilla.cantidad} Pil`;
+
+                // Reemplaza las horas
+                const horaInicio = new Date(`1970-01-01T${pastilla.hora}:00`);
+                const horaFin = new Date(horaInicio.getTime() + 60 * 60 * 1000); // Suma una hora
+
+                horaElements[0].textContent = `${horaInicio.getHours().toString().padStart(2, '0')}:00`;
+                horaElements[1].textContent = `${horaFin.getHours().toString().padStart(2, '0')}:00`;
 
                 // Añadir redireccionamiento a pastilla.html
                 horarioElement.querySelector(".pastilla").addEventListener("click", () => {
@@ -32,3 +40,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .catch(error => console.error("Error al cargar el archivo JSON:", error));
 });
+    
